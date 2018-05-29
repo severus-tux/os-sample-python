e18aea3d15b39f9a0f37fba07cd66543ef1cbd68
Vishwa Prakash H V <vishwaprakash@protonmail.com>
2018-05-29 11:13:38 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -30,7 +30,7 @@ while 1:
 
     html_content = urllib.request.urlopen('http://www.cmi.ac.in//admissions/entrance-results-2018.php').read().decode('utf-8')
     #print (html_content)
-    matches = re.findall('M-', html_content) # Because Computer Science Starts as C-BLR-0006
+    matches = re.findall('C-', html_content) # Because Computer Science Starts as C-BLR-0006
 	
     if len(matches) == 0:
        print ("Yeah, Result Not Declared. Going to sleep") #will not send anything
@@ -42,12 +42,12 @@ while 1:
        if len(selected) == 0:
            
            msg="CMI Results are Out! You are NOT Selected"
-           res = sendSMS('XVuUR2zqhZE-KbZQQD4MwO10UjIpyGdAD16Wr2imtQ', '919481212950', 'TXTLCL', msg)
+           res = sendSMS('FqRMhPtOha4-hg2Tl2F7qUUllZA3qFrDLeR3wGvwP2', '918277576500', 'TXTLCL', msg)
        
        else:
            
            msg="CMI Results are Out! You are Selected :) "
-           res = sendSMS('XVuUR2zqhZE-KbZQQD4MwO10UjIpyGdAD16Wr2imtQ', '919481212950', 'TXTLCL', msg)
+           res = sendSMS('FqRMhPtOha4-hg2Tl2F7qUUllZA3qFrDLeR3wGvwP2', msg)
            
        print (res)
        quit()
